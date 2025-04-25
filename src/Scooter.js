2e20b793313e6d7c2ff4988e2e5d9795e03d4685
gabespacey <gabrielspacey@yahoo.co.uk>
2025-04-25 19:07:06 +0100

    first day work up to createScooter()

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,37 @@
+let serial = 1
+
 class Scooter {
-  // scooter code here
+
+  static nextSerial = 1;
+
+  constructor(station) {
+    this.station = station;
+    this.user = null;
+    this.serial = Scooter.nextSerial++;
+    this.charge = 100;
+    this.isBroken = false;
+  }
+
+
+  rent(user) {
+    if (this.charge > 20 && this.isBroken == false) {
+      this.station = null;
+      this.user = user;
+    } else if (this.charge > 20 && this.isBroken == true) {
+      throw new Error ('scooter needs repair.')
+    } else if (this.charge <= 20) {
+      throw new Error ('scooter needs to charge.')
+    }
+  }
+
+
+  dock(station) {
+    this.station = station;
+    this.user = null;
+  }
+
+  
 }
+
 
 module.exports = Scooter
--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -1,7 +1,65 @@
 // require the User and Scooter classes - see where they can be used in ScooterApp.js
 
+const Scooter = require('./Scooter');
+const User = require('./User');
+
 class ScooterApp {
-  // ScooterApp code here
+  constructor() {
+    this.stations = {
+      "Barclays HQ": [],
+      "Canary Wharf": [],
+      "London Bridge": []
+    };
+    this.registeredUsers = {};
+  }
+
+
+  registerUser(username, password, age) {
+    if (this.registeredUsers[username]) {
+      throw new Error('already registered')
+    }
+    if (age < 18) {
+      throw new Error('too young to register')
+    }
+
+    const user = new User(username, password, age);
+    this.registeredUsers[username] = user;
+    console.log('User has been registered');
+    return user;
+  }
+
+
+  loginUser(username, password) {
+    const user = this.registeredUsers[username];
+
+    if (!this.registeredUsers[username]) {
+      throw new Error('username or password is incorrect')
+    }
+    try {
+      user.login(password);
+      console.log('User has been logged in')
+    } catch (err) {
+      throw new Error('username or password is incorrect')
+    }
+  }
+
+
+  logoutUser(username) {
+    try {
+      const user = this.registeredUsers[username];
+      user.logout(username)
+      console.log('User is logged out')
+    } catch (err) {
+      console.log('User is not logged in')
+    }
+
+
+    createScooter(station) {
+      if (!this.stations[station]) {
+        throw new Error('no such station')
+      }
+    }
+  }
 }
 
 module.exports = ScooterApp
